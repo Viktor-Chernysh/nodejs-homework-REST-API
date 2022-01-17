c0c2a43976ff866dfd5e2a0f9aeb93e9c0bb0b56
Vitkor Chernysh <viktor.chernysh.work@gmail.com>
2022-01-17 18:04:35 +0200

    add module test

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,26 +1,30 @@
 /* eslint-disable no-unused-vars */
 import { HttpCode } from "../../lib/constant";
-import AuthService from "../../service/auth";
+import authService from "../../service/auth";
 import {
   UploadFileService,
   CloudFileStorage,
   LocalFileStorage,
 } from "../../service/file-storage";
 
-const authService = new AuthService();
-
 const registration = async (req, res, next) => {
-  const { email } = req.body;
-  const isUserExist = await authService.isUserExist(email);
-  if (isUserExist) {
-    return res.status(HttpCode.CONFLICT).json({
-      status: "error",
-      code: HttpCode.CONFLICT,
-      message: "Email is already exist",
-    });
+  try {
+    const { email } = req.body;
+    const isUserExist = await authService.isUserExist(email);
+    if (isUserExist) {
+      return res.status(HttpCode.CONFLICT).json({
+        status: "error",
+        code: HttpCode.CONFLICT,
+        message: "Email is already exist",
+      });
+    }
+    const data = await authService.create(req.body);
+    res
+      .status(HttpCode.CREATED)
+      .json({ status: "success", code: HttpCode.CREATED, data });
+  } catch (error) {
+    next(error);
   }
-  const data = await authService.create(req.body);
-  res.status(HttpCode.OK).json({ status: "success", code: HttpCode.OK, data });
 };
 
 const login = async (req, res, next) => {
--- service/auth/index.js
+++ service/auth/index.js
@@ -36,4 +36,4 @@ class AuthService {
   }
 }
 
-export default AuthService;
+export default new AuthService();
